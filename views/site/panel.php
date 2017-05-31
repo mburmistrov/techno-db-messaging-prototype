6452de79a8a0280402fb1f9a49b9492d0cfde250
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2017-05-31 10:55:08 -0400

    Add API description to panel view

--- views/site/panel.php
+++ views/site/panel.php
@@ -25,6 +25,26 @@
               </ul>
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>API</h2>
+                <?php $conversationExampleId = mt_rand(0, 100);?>
+                <b><i>Send Mesaage</b></i><br>
+                <b>url:</b> https://technodb.mburmistrov.ru/message/send-message<br>
+                <b>example:</b> {"user_id": <?= Yii::$app->user->identity->id ?>, "conversation_id": <?= $conversationExampleId ?>, "participants": [<?= Yii::$app->user->identity->id ?>, 2], "message": "Hello 2-nd user!", "auth_key": "<?= Yii::$app->user->identity->auth_key ?>"}
+                <hr>
+                <b><i>Get Conversation</b></i><br>
+                <b>url:</b> https://technodb.mburmistrov.ru/message/get-conversation<br>
+                <b>return:</b> conversation data and messages from both persistent and operative layers<br>
+                <b>example:</b> {"user_id": <?= Yii::$app->user->identity->id ?>, "conversation_id": <?= $conversationExampleId ?>,  "auth_key": "<?= Yii::$app->user->identity->auth_key ?>"}
+                <hr>
+                <b><i>Transfer data from operative to persistent layer</b></i><br>
+                <b>url:</b> https://technodb.mburmistrov.ru/message/transfer-operative-to-persistent<br>
+                <b>example:</b> {"user_id": <?= Yii::$app->user->identity->id ?>, "auth_key": "<?= Yii::$app->user->identity->auth_key ?>"}
+                <hr>
+            </div>
 
+        </div>
     </div>
 </div>
